86485bb79eb729c9bafa0cd9dcdbade2bf599766
Aziz858 <jangaboyevazizbek@gmail.com>
2020-12-16 15:48:37 +0500

    homework

---
+++ script.js
@@ -0,0 +1,28 @@
+var isimingiz = 'Mening ismim'
+var isim = prompt('Ismingiz')
+console.log(isimingiz + ' ' + isim);
+
+var menyosh = 'Men'
+var yosh = prompt('Yoshingiz')
+var yoshdaman = 'yoshdaman'
+console.log(menyosh + ' ' + yosh + ' ' + yoshdaman);
+
+var misol1 = 'Misol 1: 20 + 10 = 30 Sizning javobingiz'
+var yechim1 = prompt('20 + 10')
+console.log(misol1 + ' ' + yechim1)
+
+var misol2 = 'Misol 2: 50 - 60 = -10 Sizning javobingiz'
+var yechim2 = prompt('50 - 60')
+console.log(misol2 + ' ' + yechim2)
+
+var misol3 = 'Misol 3: 25 * 3 = 75 Sizning javobingiz'
+var yechim3 = prompt('25 * 3')
+console.log(misol3 + ' ' + yechim3)
+
+var misol4 = 'Misol 4: 60 : 3 = 20 Sizning javobingiz'
+var yechim4 = prompt('60 : 3')
+console.log(misol4 + ' ' + yechim4)
+
+var misol5 = 'Misol 5: 555 % 5 = 0.90 Sizning javobingiz'
+var yechim5 = prompt('555 % 5')
+console.log(misol5 + ' ' + yechim5)
